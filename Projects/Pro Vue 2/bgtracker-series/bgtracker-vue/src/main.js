24142f55ab411f3efb2d43d9581838638a117a4a
urakovaliaskar <urakov.aliaskar@gmail.com>
2020-05-29 17:48:37 +0600

    Bootstrap and axios added

--- Projects/Pro Vue 2/bgtracker-series/bgtracker-vue/src/main.js
+++ Projects/Pro Vue 2/bgtracker-series/bgtracker-vue/src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './components/App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './assets/style.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
